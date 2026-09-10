74e218ab3cb1ace5dfc6cb0c8a6b5a2d2958b146
vamsim17 <vamsimarrapu17@gmail.com>
2026-09-10 16:05:26 -0500

    made the text bigger

--- lib/main.dart
+++ lib/main.dart
@@ -104,10 +104,16 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: .center,
           children: [
-            const Text('Vamsi has pushed the button this many times:'),
+            const Text('Vamsi has pushed the button this many times:', style: TextStyle(
+              fontSize: 50.0, // Adjust this number to make it bigger
+              fontWeight: FontWeight.bold, // Optional: makes it bold too
+            ),),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              style: TextStyle(
+                fontSize: 124.0, // Adjust this number to make it bigger
+                fontWeight: FontWeight.bold, // Optional: makes it bold too
+              ),
             ),
           ],
         ),
